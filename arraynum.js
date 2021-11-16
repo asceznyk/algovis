828b4c4d8d8782cb89ef91eee425ad1f2717cdf0
asceznyk <asceznyk@gmail.com>
2021-11-16 14:58:01 +0530

    ..

--- arraynum.js
+++ arraynum.js
@@ -1,22 +1,35 @@
 //given A of length N find smallest positive integer that does not occur in A assuming each element in A is in range [-1M, 1M] and N is in range [1, 100K]
 
-function createArr(r1, r2) {
-    let arr = [];
-    for(let i = r1; i < r2+1; i++) {
-        arr.push(i);
-    }
-    return arr;
-}
+function uniqSort (arr = []) {
+   const map = {};
+   const res = [];
+   for (let i = 0; i < arr.length; i++) {
+      if (!map[arr[i]]) {
+         map[arr[i]] = true;
+         res.push(arr[i]);
+      };
+   };
+    return res.sort((a, b) => a - b);
+};
 
 function solution(A) {
-    let N = A.length < 100000 ? A.length : 100000;
-    for (let b = 1; b < N+1; b++) {
-        if (!A.includes(b) || Math.max(...A) < 0){
-            return b;
-        }
+    let c = 1;
+    A = uniqSort(A);
+    console.log(A)
+    let N = Math.min(A.length, 100000);
+    for (let b = 0; b < N; b++) {
+        if(A[b] > 0) {
+            if(A[b] != c) {
+                return c;
+            }
+            c += 1;
+        } 
     }
-    return Math.max(...A) + 1;
+    return c;
 }
 
-let b = solution(createArr(1, 100000))
-console.log(b)
+let d = solution([...Array(100000).keys()])
+let e = solution([1, 3, 6, 4, 1, 2])
+let f = solution([-1,-3, 1, 2, 3, 4, 5])
+
+console.log(d, e, f)
